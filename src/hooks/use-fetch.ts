246e1ca0c46955e72d8d8848beeaf01d23abf02f
cod3prod <cod3prod@gmail.com>
2024-12-06 11:56:13 +0900

    🐛 Fix useFetch

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { Article } from "@/types/news";
+import { Article, NewsResponse } from "@/types/news";
 import { useState } from "react";
 
 export default function useFetch() {
@@ -15,12 +15,23 @@ export default function useFetch() {
   const fetchData = async () => {
     try {
       setIsLoading(true);
+      setIsError(false);
+      
       const res = await fetch(endpoint);
-      const data = await res.json();
-      setData(data.articles);
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      
+      const responseData = (await res.json()) as NewsResponse;
+      if (responseData?.articles && Array.isArray(responseData.articles)) {
+        setData(responseData.articles);
+      } else {
+        setData([]);
+      }
     } catch (error) {
       console.error(error);
       setIsError(true);
+      setData([]);
     } finally {
       setIsLoading(false);
     }
