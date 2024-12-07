be2789ffbb50beaf76d4dd7f6af37d8dc9c24301
cod3prod <cod3prod@gmail.com>
2024-12-07 12:42:59 +0900

    💄 Add hover effects to news-item

--- src/components/news-item.tsx
+++ src/components/news-item.tsx
@@ -11,9 +11,9 @@ const NewsItem = memo(function NewsItem({
   pubDate: string;
 }) {
   return (
-    <li className="bg-white mb-2 p-4 rounded-md shadow-md">
+    <li className="bg-white mb-2 p-4 rounded-md shadow-md hover:bg-gray-100">
       <Link href={link}>
-        <h3 className="mb-2 text-lg">{title}</h3>
+        <h3 className="mb-2 text-lg hover:text-blue-500">{title}</h3>
       </Link>
       <p className="text-gray-500 text-sm">{pubDate}</p>
     </li>
